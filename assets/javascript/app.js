5a6704c400540560856974602afd6d4f3bef0624
Scars and Bullets <sbrough@gmail.com>
2019-04-11 17:40:21 -0600

    broke everything

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-//Trive questions/answers as country data object nested in an array.
+//Countries for questions and answers
 var countries = [
 	(afghanistan = {
 		name: "Afghanistan",
@@ -46,16 +46,10 @@ var countries = [
 		cities: ["Manchester", "Liverpool", "York"]
 	})
 ];
-// //test trivia question data
-// console.log("Country: " + countries[0].name);
-// console.log("Capital: " + countries[0].capital);
-// console.log("City 1: " + countries[0].cities[0]);
-// console.log("City 2: " + countries[0].cities[1]);
-// console.log("City 3: " + countries[0].cities[2]);
 
 //timer variables
-var timeLimit;
-var intervalID;
+var questionTime = 15;
+var questionTimer;
 
 //holds country string value for each question
 var countryNum;
@@ -63,71 +57,75 @@ var countryNum;
 //array log countries as they are picked, prevents repeat questions
 var pickedCountries = [];
 
-// array for cities and captils, allows for authentication of correct/incorrect answers
-var capitals = [];
-var cities = [];
-
-// // answers array, generated for each question
-// var answers = [];
-
 //Score
 var correct = 0;
 var incorrect = 0;
 
 //Doc ready
-$(document).ready(runGame);
-
-function runGame() {
-	buildarrays();
-	startUp();
-}
-
-//builds capitals/cities arrays
-function buildarrays() {
-	for (var i = 0; i < countries.length; i++) {
-		capitals.push(countries[i].capital);
-		for (var j = 0; j < countries[i].cities.length; j++) {
-			cities.push(countries[i].cities[j]);
-		}
-	}
-}
-// console.log(`Capitals array: ${capitals}`);
-// console.log(`Cities array: ${cities}`);
+$(document).ready(startUp);
 
 //dynamically loads start screen and prompts user to start the game
 function startUp() {
-	setTimeLimit();
 	correct = 0;
 	incorrect = 0;
-	var mainContent = `<h1>To begin playing, click the start button. You will have ${timeLimit} seconds to answer each question.</h1><button type="button" class="btn btn-primary btn-lg">Start</button>`;
-	$("#main").empty();
-	$("#main").append(mainContent);
-	$(".btn").click(question); //run function question()
+	var mainContent = `<h1>To begin playing, click the start button. You will have ${questionTime} seconds to answer each question.</h1><button type="button" class="btn btn-primary btn-lg">Start</button>`;
+	$("#main").html(mainContent);
+	$(".btn").click(question);
 }
 
-//dynamically loads the trivia question, timer, and row for answers.
 function question() {
-	countryNum = pickCountry(); //countryNum assigned unique "rando" number from pickCountry
+	//Determin which country will be used for the question and stores it
+	countryNum = pickCountry();
 	pickedCountries.push(countries[countryNum].name);
+
+	//Display Score
 	$("#score").html(`<h1 class="display-4">Your answers:</h1>
 		<hr>Correct - ${correct}<br>Incorrect - ${incorrect}`);
+
+	//Builds question screen layout
 	var layout = `<div class="row"><div class="col-lg-8" id="left"></div><div class="col-lg-4" id="right"></div></div><div class="row" id="answers"></div>`;
-	setTimeLimit();
-	intervalID = setInterval(timer, 1000);
 	$("#main").empty();
 	$("#main").append(layout);
+
+	//Displays questions
 	$("#left").append(
 		`<h1>What is the capital of ${countries[countryNum].name}?`
 	);
-	$("#right").append(`<h1 id="timer">Time left: ${timeLimit}`);
+
+	//Start 15 second question timer
+	questionTime = 15;
+	questionTimer = setInterval(timer, 1000);
+
+	//Displays time remaining to answer question
+	$("#right").append(`<h1 id="timer">Time left: ${questionTime}`);
+
+	//dynamically loads answers on screen
 	loadAnswers();
 }
 
-//Determines if game is over or not
-//Pick a random country from the countries array that hasn't been called before
-//Returns "rando", which is the index for the unique country
+function timer() {
+	if (questionTime <= 0) {
+		clearInterval(questionTimer);
+		timeRanOut();
+	} else {
+		$("#timer").text(`Time left: ${questionTime}`);
+	}
+}
+
+function timeRanOut() {
+	$("#timer").text(`Time's up!`);
+	$("#answers").empty();
+	$("#left").html(
+		`<h1>You ran out of time. The capital of ${
+			countries[countryNum].name
+		} is ${countries[countryNum].capital}.`
+	);
+	incorrect++;
+	setTimeout(question, 5000);
+}
+
+//picks unique country from array
 function pickCountry() {
-	//keeps picking new country/question until all have been picked
 	if (pickedCountries.length < countries.length) {
 		var unique = false;
 		while (!unique) {
@@ -143,28 +141,6 @@ function pickCountry() {
 		}
 	} else {
 		gameOver();
-	}
-}
-
-function setTimeLimit() {
-	timeLimit = 15;
-}
-
-function timer() {
-	timeLimit--;
-	if (timeLimit <= 0) {
-		incorrect++;
-		clearInterval(intervalID);
-		$("#timer").text(`Time's up!`);
-		$("#answers").empty();
-		$("#left").html(
-			`<h1>You ran out of time. The capital of ${
-				countries[countryNum].name
-			} is ${countries[countryNum].capital}.`
-		);
-		setTimeout(question, 5000);
-	} else {
-		$("#timer").text(`Time left: ${timeLimit}`);
 	}
 }
 
@@ -190,15 +166,15 @@ function loadAnswers() {
 }
 
 function validateAnswer(input) {
+	clearInterval(questionTimer);
 	$("#answers").empty();
-	if (capitals.includes(input)) {
+	if (countries[countryNum].capital === input) {
 		correct++;
 		$("#left").html(
 			`<h1>That is correct! ${
 				countries[countryNum].capital
 			} is the captial of ${countries[countryNum].name}.`
 		);
-		setTimeout(question, 5000);
 	} else {
 		incorrect++;
 		$("#left").html(
@@ -206,8 +182,8 @@ function validateAnswer(input) {
 				countries[countryNum].capital
 			} is the captial of ${countries[countryNum].name}.`
 		);
-		setTimeout(question, 5000);
 	}
+	setTimeout(question, 5000);
 }
 
 //Fisher-Yates Shuffle from: https://javascript.info/task/shuffle, applied to shuffle my answers
